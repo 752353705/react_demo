94ae7b36e3be2b902b0428441f4f39861c1d0bb7
unknown <zhangfq15110075640@163.com>
2020-01-13 00:40:38 +0800

    123

--- src/components/Line/line.js
+++ src/components/Line/line.js
@@ -1,28 +1,48 @@
 import React,{Component} from "react"
 import {Card} from "antd"
 import ReactEcharts from "echarts-for-react"
+
 class Line extends Component {
     constructor(){
         super()
+        
         this.state={
-           
+            
             option:{
                 xAxis: {
                     type: 'category',
                     boundaryGap: false,
-                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                    data: ['2月20日', '2月21日', '2月22日', '2月23日', '2月24日', '2月25日', '2月26日']
                 },
                 yAxis: {
                     type: 'value'
                 },
+             
+                tooltip: {
+                    trigger: 'axis'
+                },
                 series: [{
-                    data: [820, 932, 901, 934, 1290, 1330, 1320],
+                    data: [310, 932, 91, 93, 1290, 130, 130],
                     type: 'line',
-                    areaStyle: {}
+                    boundaryGap: true,
+                    symbolSize:14,
+                    markLine: {
+                        data: [
+                            {type: 'average', name: '平均值'}
+                        ]
+                    },
+                    areaStyle: {
+                        width:"70%",
+                        color:"skyblue"
+                    }
                 }]
             }
         }
     }
+
+   
+
+
     render(){
         let {option} = this.state;
         console.log(option)
